1a5814be49435333fa3501c87201b8116dfd7127
Demid Mishukov <mishukov1337mail.ru@gmail.com>
2024-03-22 20:39:31 +0500

    commit to a new branch

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ title.before('what? before?') // перед элементом
 title.prepend('Hi!!!!!!!') // в элементе в начале 
 title.append(',Demid Mishukov') // в элементе в конце
 title.after('what? after?')// после элемента
+
+
+console.log('я сделал! ')
