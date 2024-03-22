0d9e2f0dd90ee783125ee38016cb36ff01f109e7
Demid Mishukov <mishukov1337mail.ru@gmail.com>
2024-03-22 20:35:33 +0500

    added text before , after the h1 tag and even put some text inside

---
+++ script.js
@@ -0,0 +1,10 @@
+let title = document.querySelector('h1')
+console.log('hello')
+const newEl = document.createElement('div')
+newEl.innerText = 'hui'
+
+
+title.before('what? before?') // перед элементом
+title.prepend('Hi!!!!!!!') // в элементе в начале 
+title.append(',Demid Mishukov') // в элементе в конце
+title.after('what? after?')// после элемента
